af145a469926810f02e753a97e769890a1b9a7d7
Lauren Carne <oreocookie@hotmail.co.uk>
2019-04-02 10:44:15 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,8 @@
 def hello_t(array)
-  array.each {|name| if name.start_with("T")
-  puts "Hi, #{name}"}
+  array.each do |name| if name.start_with("T")
+  puts "Hi, #{name}"
+end
 end
 
 # call your method here!
+
